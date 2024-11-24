4b9a717711a334210bdd5fd96176671fca80fabe
guojiang <guojiangf@outlook.com>
2024-11-24 16:45:40 +0800

    first commit

---
+++ java_01/com/gjy/demo/Demo.java
@@ -0,0 +1,10 @@
+/**
+ * author: guojiang
+ * date: 2024/11/24 16:44
+ * description: First Demo
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
